c122a26df311bd76a53e842960f824987cb77f0f
ac-89 <alancarroll89@gmail.com>
2023-05-14 11:09:45 +0100

    Class 26

---
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -0,0 +1,11 @@
+//Create a pizza object that has four properties and three methods
+
+let Pizza = {};
+
+Pizza.cheese = true;
+Pizza.stuffedCrust = false;
+Pizza.slices = 8;
+Pizza.width = 12;
+Pizza.isTasty = function (value) {
+  console.log("yummy!");
+};
